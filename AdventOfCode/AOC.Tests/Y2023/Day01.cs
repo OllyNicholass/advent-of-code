9ce0529a4d498543e435cb1d188e4b8e93ad70a9
Olly Nicholass <OllyNicholass@users.noreply.github.com>
2023-12-01 20:45:26 +0000

    feat(23.1): add part 2 solution

--- AdventOfCode/AOC.Tests/Y2023/Day01.cs
+++ AdventOfCode/AOC.Tests/Y2023/Day01.cs
@@ -42,5 +42,74 @@ namespace AOC.Tests.Y2023
 
             Console.WriteLine($"Part 1: {result}");
         }
+
+        [TestCase(@"two1nine
+            eightwothree
+            abcone2threexyz
+            xtwone3four
+            4nineeightseven2
+            zoneight234
+            7pqrstsixteen", 281)]
+        [TestCase(null, 54094)] // The actual answer
+        public void Part2(string input, int? expected)
+        {
+            string[] lines = input != null ? input.Split("\n") : realData;
+
+            int result = lines.Sum(line =>
+            {
+                var digits = new List<int>();
+
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (char.IsDigit(line[i]))
+                    {
+                        digits.Add(int.Parse(line[i].ToString()));
+                    }
+                    else
+                    {
+                        foreach (var spelledOutNumber in spelledOutDigits.Keys.OrderByDescending(s => s.Length))
+                        {
+                            if (i + spelledOutNumber.Length <= line.Length && line.Substring(i, spelledOutNumber.Length) == spelledOutNumber)
+                            {
+                                digits.Add(spelledOutDigits[spelledOutNumber]);
+                                i += spelledOutNumber.Length - 2; // Skip the last character of the spelled-out number incase of overlap
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                // Calculate the result for the current line
+                int lineResult = digits.Count switch
+                {
+                    1 => Convert.ToInt32($"{digits[0]}{digits[0]}"),
+                    > 1 => Convert.ToInt32($"{digits[0]}{digits[^1]}"),
+                    _ => throw new Exception("No digits")
+                };
+
+                return lineResult;
+            });
+
+            if (expected != null)
+            {
+                Assert.That(result, Is.EqualTo(expected.Value));
+            }
+
+            Console.WriteLine($"Part 2: {result}");
+        }
+
+        // Dictionary to map spelled-out digits to their numeric values
+        private static readonly Dictionary<string, int> spelledOutDigits = new Dictionary<string, int>
+        {
+            { "one", 1 },
+            { "two", 2 },
+            { "three", 3 },
+            { "four", 4 },
+            { "five", 5 },
+            { "six", 6 },
+            { "seven", 7 },
+            { "eight", 8 },
+            { "nine", 9 }
+        };
     }
 }
